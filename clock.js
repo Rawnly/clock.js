4c82b812015f730eb52e5b8726b756432a0819ce
Federico Vitale <fedevitale99@gmail.com>
2016-07-07 13:40:14 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,29 +1,54 @@
-function Clock() {
-    this.new = function (selector) {
-      setInterval(function () {
-        var d = new Date();
-        var day = {
-          h: d.getHours(),
-          m: d.getMinutes(),
-          s: d.getSeconds(),
-          ms: d.getMilliseconds()
-        };
+  function Clock() {
+    this.new = function (selector, tf) {
+      if (tf == 'true') {
+        setInterval(function () {
+          var d = new Date();
+          var day = {
+            h: d.getHours(),
+            m: d.getMinutes(),
+            s: d.getSeconds(),
+            ms: d.getMilliseconds()
+          };
 
-        day.h = check(day.h);
-        day.m = check(day.m);
-        day.s = check(day.s);
-        day.ms = check(day.ms);
+          day.h = check(day.h);
+          day.m = check(day.m);
+          day.s = check(day.s);
+          day.ms = check(day.ms);
 
-        function check(i) {
-          if (i < 0) {i = "0" + i;}
-          return i;
-        }
+          function check(i) {
+            if (i < 0) {i = "0" + i;}
+            return i;
+          }
 
-        var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
-        var hexTime = '#' + day.h + '' + day.m + '' + day.s;
+          var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
+          var hexTime = '#' + day.h + '' + day.m + '' + day.s;
 
-        $(selector).html(t);
-      }, 1);
+          $(selector).html(t);
+        }, 1);
+      } else {
+        setInterval(function () {
+          var d = new Date();
+          var day = {
+            h: d.getHours(),
+            m: d.getMinutes(),
+            s: d.getSeconds()
+          };
+
+          day.h = check(day.h);
+          day.m = check(day.m);
+          day.s = check(day.s);
+
+          function check(i) {
+            if (i < 0) {i = "0" + i;}
+            return i;
+          }
+
+          var t = day.h + ":" + day.m + ":" + day.s;
+          var hexTime = '#' + day.h + '' + day.m + '' + day.s;
+
+          $(selector).html(t);
+        }, 1);
+      }
     };
     this.setBackground = function (element) {
       setInterval(function () {
@@ -31,14 +56,12 @@ function Clock() {
         var day = {
           h: d.getHours(),
           m: d.getMinutes(),
-          s: d.getSeconds(),
-          ms: d.getMilliseconds()
+          s: d.getSeconds()
         };
 
         day.h = check(day.h);
         day.m = check(day.m);
         day.s = check(day.s);
-        day.ms = check(day.ms);
 
         function check(i) {
           if (i < 0) {i = "0" + i;}
@@ -57,14 +80,12 @@ function Clock() {
         var day = {
           h: d.getHours(),
           m: d.getMinutes(),
-          s: d.getSeconds(),
-          ms: d.getMilliseconds()
+          s: d.getSeconds()
         };
 
         day.h = check(day.h);
         day.m = check(day.m);
         day.s = check(day.s);
-        day.ms = check(day.ms);
 
         function check(i) {
           if (i < 0) {i = "0" + i;}
