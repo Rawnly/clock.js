cbde930b38aeece05aed0045d185a1432656dcc1
Federico Vitale <fedevitale99@gmail.com>
2016-07-07 14:26:41 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,110 +1,59 @@
-  function Clock() {
-    this.new = function (selector, tf, type) {
-      if (tf === true) {
-        setInterval(function () {
-          var d = new Date();
-          var day = {
-            h: d.getHours(),
-            m: d.getMinutes(),
-            s: d.getSeconds(),
-            ms: d.getMilliseconds()
-          };
-
-          day.h = check(day.h);
-          day.m = check(day.m);
-          day.s = check(day.s);
-          day.ms = check(day.ms);
-
-          function check(i) {
-            if (i < 0) {i = "0" + i;}
-            return i;
-          }
-
-          var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
-          var hexTime = '#' + day.h + '' + day.m + '' + day.s;
-
-          if (type === false || type === undefined) {
-            $(selector).html(t);
-          } else if (type === true) {
-            $(selector).html(hexTime);
-          }
-
-        }, 1);
-      } else {
-        setInterval(function () {
-          var d = new Date();
-          var day = {
-            h: d.getHours(),
-            m: d.getMinutes(),
-            s: d.getSeconds()
-          };
-
-          day.h = check(day.h);
-          day.m = check(day.m);
-          day.s = check(day.s);
-
-          function check(i) {
-            if (i < 0) {i = "0" + i;}
-            return i;
-          }
-
-          var t = day.h + ":" + day.m + ":" + day.s;
-          var hexTime = '#' + day.h + '' + day.m + '' + day.s;
-
-          if (type === false || type === undefined) {
-            $(selector).html(t);
-          } else if (type === true) {
-            $(selector).html(hexTime);
-          }
-        }, 1);
-      }
+function Clock() {
+  function temp() {
+    d = new Date();
+    day = {
+      h: d.getHours(),
+      m: d.getMinutes(),
+      s: d.getSeconds(),
+      ms: d.getMilliseconds()
     };
-    this.setBackground = function (element) {
+
+    day.h = check(day.h);
+    day.m = check(day.m);
+    day.s = check(day.s);
+    day.ms = check(day.ms);
+
+    function check(i) {
+      if (i < 0) {i = "0" + i;}
+      return i;
+    }
+
+    t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
+    hexTime = '#' + day.h + '' + day.m + '' + day.s;
+  }
+
+  this.new = function (selector, tf, type) {
+    temp();
+    if (tf === true) {
       setInterval(function () {
-        var d = new Date();
-        var day = {
-          h: d.getHours(),
-          m: d.getMinutes(),
-          s: d.getSeconds()
-        };
-
-        day.h = check(day.h);
-        day.m = check(day.m);
-        day.s = check(day.s);
-
-        function check(i) {
-          if (i < 0) {i = "0" + i;}
-          return i;
+        if (type === false || type === undefined) {
+          $(selector).html(t);
+        } else if (type === true && ts === false) {
+          $(selector).html(hexTime);
+        } else {
+          console.error('Maybe you set true ms, disable it to display hexcode');
         }
-
-        var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
-        var hexTime = '#' + day.h + '' + day.m + '' + day.s;
-
-        $(element).css('background-color', hexTime);
       }, 1);
-    };
-    this.setColor = function (element) {
+    } else {
       setInterval(function () {
-        var d = new Date();
-        var day = {
-          h: d.getHours(),
-          m: d.getMinutes(),
-          s: d.getSeconds()
-        };
-
-        day.h = check(day.h);
-        day.m = check(day.m);
-        day.s = check(day.s);
-
-        function check(i) {
-          if (i < 0) {i = "0" + i;}
-          return i;
+        temp();
+        if (type === false || type === undefined) {
+          $(selector).html(t);
+        } else if (type === true) {
+          $(selector).html(hexTime);
         }
-
-        var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
-        var hexTime = '#' + day.h + '' + day.m + '' + day.s;
-
-        $(element).css('color', hexTime);
       }, 1);
+    }
   };
+  this.setBackground = function (element) {
+    setInterval(function () {
+      temp();
+      $(element).css('background-color', hexTime);
+    }, 1);
+  };
+  this.setColor = function (element) {
+    setInterval(function () {
+      $(element).css('color', hexTime);
+    }, 1);
+};
 }
