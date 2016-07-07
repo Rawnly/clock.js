80de46d7f4994e0e065cb32e3fad6db943ccb6fa
Federico Vitale <fedevitale99@gmail.com>
2016-07-07 13:20:18 +0200

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,80 @@
+function Clock() {
+    this.createClock = function (selector) {
+      setInterval(function () {
+        var d = new Date();
+        var day = {
+          h: d.getHours(),
+          m: d.getMinutes(),
+          s: d.getSeconds(),
+          ms: d.getMilliseconds()
+        };
+
+        day.h = check(day.h);
+        day.m = check(day.m);
+        day.s = check(day.s);
+        day.ms = check(day.ms);
+
+        function check(i) {
+          if (i < 0) {i = "0" + i;}
+          return i;
+        }
+
+        var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
+        var hexTime = '#' + day.h + '' + day.m + '' + day.s;
+
+        $(selector).html(t);
+      }, 1);
+    };
+    this.setBackground = function (element) {
+      setInterval(function () {
+        var d = new Date();
+        var day = {
+          h: d.getHours(),
+          m: d.getMinutes(),
+          s: d.getSeconds(),
+          ms: d.getMilliseconds()
+        };
+
+        day.h = check(day.h);
+        day.m = check(day.m);
+        day.s = check(day.s);
+        day.ms = check(day.ms);
+
+        function check(i) {
+          if (i < 0) {i = "0" + i;}
+          return i;
+        }
+
+        var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
+        var hexTime = '#' + day.h + '' + day.m + '' + day.s;
+
+        $(element).css('background-color', hexTime);
+      }, 1);
+    };
+    this.setColor = function (element) {
+      setInterval(function () {
+        var d = new Date();
+        var day = {
+          h: d.getHours(),
+          m: d.getMinutes(),
+          s: d.getSeconds(),
+          ms: d.getMilliseconds()
+        };
+
+        day.h = check(day.h);
+        day.m = check(day.m);
+        day.s = check(day.s);
+        day.ms = check(day.ms);
+
+        function check(i) {
+          if (i < 0) {i = "0" + i;}
+          return i;
+        }
+
+        var t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
+        var hexTime = '#' + day.h + '' + day.m + '' + day.s;
+
+        $(element).css('color', hexTime);
+      }, 1);
+  };
+}
