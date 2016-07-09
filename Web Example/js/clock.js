828c868166ffd64577f45fe6ba1680f6dc58eed1
Federico Vitale <fedevitale99@gmail.com>
2016-07-10 01:46:34 +0200

    Update clock.js

--- Web Example/js/clock.js
+++ Web Example/js/clock.js
@@ -1,59 +1,59 @@
-function Clock() {
-  function temp() {
-    d = new Date();
-    day = {
-      h: d.getHours(),
-      m: d.getMinutes(),
-      s: d.getSeconds(),
-      ms: d.getMilliseconds()
-    };
+(function( $ ) {
+  $.fn.newClock = function ( options ) {
 
-    day.h = check(day.h);
-    day.m = check(day.m);
-    day.s = check(day.s);
-    day.ms = check(day.ms);
+    var t, tNoMs, hexTime, element; // here we making this variables local to current jQuery object
 
-    function check(i) {
-      if (i < 0) {i = "0" + i;}
-      return i;
-    }
+    function time() {
+      d = new Date();
+      day = {
+        h: d.getHours(),
+        m: d.getMinutes(),
+        s: d.getSeconds(),
+        ms: d.getMilliseconds()
+      };
 
-    t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
-    tNoMs = day.h + ":" + day.m + ":" + day.s;
-    hexTime = '#' + day.h + '' + day.m + '' + day.s;
-  }
-
-  this.new = function (selector, tf, type) {
-    temp();
-    function getStatus(variable) {
-      if (variable === true) {
-        return variable;
-      } else {
-        return variable;
-      }
-    }
-    setInterval(function () {
-      if (tf === true) {
-        $(selector).html(t);
-      } else {
-        $(selector).html(tNoMs);
+      function check(i) {
+        if (i < 10) {i = "0" + i;}
+        return i;
       }
 
-      if (type === true) {
-        $(selector).html(hexTime);
+      day.h = check(day.h);
+      day.m = check(day.m);
+      day.s = check(day.s);
+
+      t = day.h + ":" + day.m + ":" + day.s + ":" + day.ms;
+      tNoMs = day.h + ":" + day.m + ":" + day.s;
+      hexTime = '#' + day.h + '' + day.m + '' + day.s;
+    }
+
+    // opt must be local too
+    var opt = $.extend({
+      ms: false,
+      type: 'classic'
+    }, options );
+
+    element = this; //you don't need here $(this)
+
+    var timer = setInterval(function () {
+      time();
+      switch (opt.ms) {
+        case true:
+          time();
+          element.html(t);
+          break;
+        case false:
+          switch (opt.type) {
+            case 'classic':
+              time();
+              element.html(tNoMs);
+              break;
+            case 'hex':
+              time();
+              element.html(hexTime);
+              break;
+          }
+          break;
       }
     }, 1);
   };
-  this.setBackground = function (element) {
-    setInterval(function () {
-      temp();
-      $(element).css('background-color', hexTime);
-    }, 1);
-  };
-  this.setColor = function (element) {
-    temp();
-    setInterval(function () {
-      $(element).css('color', hexTime);
-    }, 1);
-};
-}
+} ( jQuery ) );
